ae9c7d4a11397ae2b7dfee6d579784657756a738
Kiki Dawson <kikiadawson@gmail.com>
2024-03-17 11:20:50 +1300

    checkov

--- github.tf
+++ github.tf
@@ -3,6 +3,9 @@ data "github_repository" "tf_aws_template_contact_form" {
 }
 
 resource "github_branch_protection" "main" {
+  #checkov:skip=CKV_GIT_6: requires signed commits
+  #checkov:skip=CKV_GIT_5: needs at least 2 approvals but I'm a party of 1 so no.
+
   repository_id = data.github_repository.tf_aws_template_contact_form.node_id
 
   pattern                         = "main"
